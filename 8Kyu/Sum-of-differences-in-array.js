727a41bf05a8afc17811169028ab0ff6c233cd7f
stephane job <darksh3ll.js@gmail.com>
2018-09-15 20:21:15 +0200

    ssss

--- 8Kyu/Sum-of-differences-in-array.js
+++ 8Kyu/Sum-of-differences-in-array.js
@@ -1,8 +1,7 @@
 const score = [10,2,1];
+
 function sumOfDifferences(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        return arr[i]
-    }
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
 }
 
 console.log(sumOfDifferences(score))
